ed411d64c36847e47b60f8da93561b50f3a170f8
Zynomon aelius <92979020+zynomon@users.noreply.github.com>
2026-03-28 13:44:05 +0600

    Update NNS26.sh

--- NNS26.sh
+++ NNS26.sh
@@ -3,7 +3,7 @@
 sudo rm -f /etc/apt/sources.list.d/debian-live-media.list
 sudo apt-get update
 sudo apt remove -y --purge calamares calamares-settings-error partitionmanager squashfs-tools live-task-recommended live-task-localisation live-boot live-boot-doc live-config live-config-doc live-config-systemd live-tools 
-sudo apt install once -y && /bin/once ; sudo apt remove once
+sudo apt install once -y && /bin/once ; sudo apt remove once -y
 sudo apt autoremove -y
 
 
